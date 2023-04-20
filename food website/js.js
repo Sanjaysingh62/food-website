ddbb660f7cc0fbc7d5cf17caff22a2cc01b8d56e
Sanjay Singh Rathore <126376580+Sanjaysingh62@users.noreply.github.com>
2023-04-20 10:55:19 +0530

    Add files via upload

---
+++ food website/js.js
@@ -0,0 +1,77 @@
+var menu = document.querySelector("#menu-btn");
+var navbar = document.querySelector(".navbar");
+var search = document.querySelector(".search-form");
+var shopping = document.querySelector(".shopping-cart");
+var login = document.querySelector(".login-form");
+var table = document.querySelector(".booking-table");
+var icon = document.querySelector(".icons");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+  login.classList.remove("active");
+  shopping.classList.remove("active");
+  search.classList.remove("active");
+  table.classList.remove("active");
+  icon.classList.toggle("active");
+};
+
+document.querySelector("#booked").onclick = () => {
+  table.classList.toggle("active");
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  login.classList.remove("active");
+  shopping.classList.remove("active");
+  search.classList.remove("active");
+};
+
+document.querySelector("#search-btn").onclick = () => {
+  search.classList.toggle("active");
+  login.classList.remove("active");
+  navbar.classList.remove("active");
+  shopping.classList.remove("active");
+  table.classList.remove("active");
+};
+document.querySelector("#shopping-btn").onclick = () => {
+  shopping.classList.toggle("active");
+  search.classList.remove("active");
+  navbar.classList.remove("active");
+  login.classList.remove("active");
+  table.classList.remove("active");
+};
+document.querySelector("#user-btn").onclick = () => {
+  login.classList.toggle("active");
+  shopping.classList.remove("active");
+  search.classList.remove("active");
+  navbar.classList.remove("active");
+  table.classList.remove("active");
+};
+
+window.onscroll = () => {
+  login.classList.remove("active");
+  shopping.classList.remove("active");
+  search.classList.remove("active");
+  navbar.classList.remove("active");
+  table.classList.remove("active");
+  menu.classList.remove("fa-times");
+};
+
+var swiper = new Swiper(".dishes-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
